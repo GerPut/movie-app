89cf32e3d66bbd6c527a089dbe665876b1291ceb
Gerhard <gerhard.putter@yahoo.com>
2021-10-20 09:08:22 +0200

    Dynamic search value in Api.

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,7 +3,7 @@ import React from 'react'
 const SearchBox = (props) => {
     return (
         <div className='col col-sm-4'>
-            <input className="form-control" placeholder="Type to search"></input>
+            <input value={props.value} onChange={(e) => props.setSearchValue(e.target.value)} className="form-control" placeholder="Type to search"></input>
         </div>
     )
 }
